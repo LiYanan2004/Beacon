5d9c1689ebb608ab0abf8264ff3c0c033631b0be
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2024-03-14 21:14:01 +0800

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by LiYanan2004 on 2024/3/14.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
